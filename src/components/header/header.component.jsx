1a0e68a4b2d07186f2d649b98aa6473c9017370f
Tarun yadav <56935638+tarunyadav1@users.noreply.github.com>
2020-10-01 23:14:04 +0530

    add title, icon, site mata data and fixed header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,36 +1,33 @@
-import React from 'react';
-import CartIcon from '../../components/cart-icon/cart-icon.component';
-import { Link } from 'react-router-dom';
-import {connect} from 'react-redux';
+import React from "react";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import './header.styles.scss';
-import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import "./header.styles.scss";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({hidden})=>(
-    <div className='header '>
-    <div className='options'>
-    <Link className='option' to='/'>
-    Home
-    </Link>
-    <Link className='option' to='/menu'>
-    Menu
-    </Link>
-    <Link className='option' to='/menu'>
-    Contact
-    </Link>
-    <CartIcon/>
+const Header = ({ hidden }) => (
+  <div className="header ">
+    <div className="options">
+      <Link className="option" to="/">
+        Home
+      </Link>
+      <Link className="option" to="/menu">
+        Menu
+      </Link>
+      <Link className="option" to="/menu">
+        Contact
+      </Link>
+      <div>
+        <CartIcon />
+      </div>
     </div>
-    {
-        hidden ? null :
-        <CartDropdown/>
-    }
-    </div>
+    {hidden ? null : <CartDropdown />}
+  </div>
 );
 
-
-const mapStateToProps = ({cart: {hidden}})=>({
-hidden
+const mapStateToProps = ({ cart: { hidden } }) => ({
+  hidden,
 });
-
 
 export default connect(mapStateToProps)(Header);
